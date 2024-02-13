9e1e5361cd6a32d725890c7d6b4ac2beb53ca38e
ammarawebs <ammar.awebs@gmail.com>
2024-02-13 20:32:15 +0500

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center bg-gray-200'>
+      <h1 className='font-sans font-semibold text-2xl'>The Real Estate App</h1>
+    </div>
+  )
+}
+
+export default App 
