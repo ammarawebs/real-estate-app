55dbcf5cbc43726478b7992a71e731494b054296
ammarawebs <ammar.awebs@gmail.com>
2024-02-16 15:17:15 +0500

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div className='flex justify-center bg-gray-200'>
-      <h1 className='font-sans font-semibold text-2xl'>The Real Estate App</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}  />
+          <Route path='/about' element={<About/>}  />
+          <Route path='/profile' element={<Profile/>}  />
+          <Route path='/sign-in' element={<SignIn/>}  />
+          <Route path='/sign-up' element={<SignUp/>}  />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
