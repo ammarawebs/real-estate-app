8bbe1191e3133f434e37106e5e043e294903c2fd
ammarawebs <ammar.awebs@gmail.com>
2024-03-27 20:16:01 +0500

    added pre installation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { IoSearch } from "react-icons/io5";
 import { useSelector } from 'react-redux';
 import { Navbar } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const currentUser = useSelector((state=>state.user))
+  const [searchTerm , setSearchTerm] = useState('')
+  const navigate = useNavigate()
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+
+  }
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+    
+  },[location.search])
+
   return (
     <header className=' bg-slate-200 flex align-middle justify-center shadow-md'>
         <div className=' p-5 text-center  flex justify-between w-full sm:w-5/6 items-center '>
@@ -13,9 +36,13 @@ const Header = () => {
                 <span className=' text-slate-500'>Real</span>
                 <span className=' text-slate-900'>Estate</span>
             </h1>
-            <form action="" className=' flex items-center bg-white p-2 rounded-lg'>
-              <input type="text" placeholder='Search...' className=' w-36 sm:w-52 rounded-lg bg-transparent outline-none' />
-              <IoSearch />
+            <form onSubmit={handleSubmit} action="" className=' flex items-center bg-white p-2 rounded-lg'>
+              <input value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)} type="text" placeholder='Search...' className=' w-36 sm:w-52 rounded-lg bg-transparent outline-none' />
+
+              <button >
+                <IoSearch />
+              </button>
+              
             </form>
             <ul className=' flex gap-5 text-sm font-semibold items-center'>
               <Link to={'/'}>
