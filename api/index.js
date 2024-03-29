8f3db4f12564a68c1891e8a76190c899769c9b6f
ammarawebs <ammar.awebs@gmail.com>
2024-03-29 21:32:48 +0500

    more

--- api/index.js
+++ api/index.js
@@ -7,10 +7,10 @@ import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import cors from 'cors';
+
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
@@ -18,23 +18,16 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
-
 const app = express();
 
-
+// CORS middleware setup
+app.use(cors({
+  origin: 'https://real-estate-app-two-gray.vercel.app', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specified HTTP methods
+  credentials: true, // Allow credentials such as cookies, authorization headers
+}));
 
 app.use(express.json());
-
-app.use(cors(
-  {
-    origin :['https://real-estate-app-two-gray.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  }
-));
-
-
 app.use(cookieParser());
 
 app.listen(3000, () => {
@@ -45,13 +38,15 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
