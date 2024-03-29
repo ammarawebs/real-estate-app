41081d35d978cf437ae3751f8064fb9f4eed9a49
ammarawebs <ammar.awebs@gmail.com>
2024-03-29 19:48:12 +0500

    disabled ssl

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://real-estate-api-tau.vercel.app',
-        secure: false,
+        target: 'http://real-estate-api-tau.vercel.app', // Use HTTP instead of HTTPS
+        secure: false, // Disable SSL verification
       },
     },
   },
