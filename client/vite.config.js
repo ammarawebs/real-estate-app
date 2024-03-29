c8bc53ff4decb50bc333e543a366f9fc50b98504
ammarawebs <ammar.awebs@gmail.com>
2024-03-29 17:18:12 +0500

    updated vite.config.js with vercel api

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://real-estate-api-tau.vercel.app',
         secure: false,
       },
     },
